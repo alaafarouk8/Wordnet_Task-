620d6f9a716bc3320873b05a351f3095720c3f1a
ALaa Farouk <alaafar19@gmail.com>
2021-05-23 07:55:28 +0200

    :100:

--- task.py
+++ task.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat May 22 22:58:17 2021
-
 @author: ALAA
 """
 import nltk
@@ -9,23 +7,26 @@ nltk.download('wordnet')
 
 from nltk.corpus import wordnet
 
-def getWord(words):
-    syn=wordnet.synsets(words)[0]
-    word=syn.hypernyms()
-    word2=word[0].lemmas()[0].name()
-    if (word2=="dog"):
-        print(words + " is a dog")
-    elif(word2=="cat"):
-        print(words + " is a cat")
-    else:
-        print(words + " is not a cat or dog")
-            
-getWord('working_dog')
-getWord('king')
-getWord('domestic_cat')
-getWord('poodle')
-getWord('leonberg')
-getWord('puppy')
+
+cat = 'cat.n.01'
+dog = 'dog.n.01'
+entity = 'entity.n.01'
+
+def getWord(k,w):
+    for ss in w:
+        for hyper in ss.hypernyms():         
+              if (hyper.name() == cat):
+                  print(k + " is a cat")
+              elif (hyper.name()==dog):
+                  print(k + " is a dog")
+              elif (hyper.name()==entity):
+                  print(k + " is not a dog or a cat")  
+              else:  
+                   return getWord(k,hyper.hypernyms())
+
+getWord('king',wordnet.synsets('king'))
+getWord('working_dog',wordnet.synsets('working_dog'))
+getWord('domestic_cat',wordnet.synsets('domestic_cat'))
 
 
 
